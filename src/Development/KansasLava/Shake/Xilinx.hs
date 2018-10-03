1fba6504495e083d427f3dfb7620d15d8d3ea5a5
Dr. ERDI Gergo <gergo@erdi.hu>
2018-10-03 17:19:09 +0200

    Take Xilinx toolchain configuration from build.mk

--- src/Development/KansasLava/Shake/Xilinx.hs
+++ src/Development/KansasLava/Shake/Xilinx.hs
@@ -1,27 +1,41 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 module Development.KansasLava.Shake.Xilinx
-       ( XilinxConfig(..), XilinxTarget(..)
+       ( XilinxTarget(..)
+       , papilioOne, papilioPro
        , xilinxRules
        ) where
 
 import           Development.Shake          hiding ((~>))
 import           Development.Shake.FilePath
+import           Development.Shake.Config
 
 import qualified Data.Text                  as T
 import           Text.Mustache
 import           Text.Mustache.Types
+import qualified Data.Map as M
+import           Data.Maybe (fromMaybe)
 
 import           Paths_kansas_lava_shake
 
-data XilinxTarget = XilinxTarget{ targetFamily, targetDevice, targetSpeed, targetPackage :: String }
+data XilinxTarget = XilinxTarget{ targetFamily, targetDevice, targetPackage, targetSpeed :: String }
 
-data XilinxConfig = XilinxConfig{ xilinxRoot   :: FilePath
-                                , xilinxTarget :: XilinxTarget
-                                }
+papilioPro :: XilinxTarget
+papilioPro = XilinxTarget "Spartan6" "xc6slx9" "tqg144" "-2"
 
-xilinxRules :: XilinxConfig -> FilePath -> String -> [FilePath] -> [FilePath] -> Rules ()
-xilinxRules XilinxConfig{..} outDir projName srcs ipcores = do
+papilioOne :: XilinxTarget
+papilioOne = XilinxTarget "Spartan3E" "xc3s500e" "vq100" "-5"
+
+boards :: M.Map String XilinxTarget
+boards = M.fromList
+    [ ("papilio-pro", papilioPro)
+    , ("papilio-one", papilioOne)
+    ]
+
+xilinxRules :: Maybe XilinxTarget -> FilePath -> String -> [FilePath] -> [FilePath] -> Rules ()
+xilinxRules mtarget outDir projName srcs ipcores = do
+    usingConfigFile "build.mk"
+
     outDir </> projName <.> "bit" %> \_out -> do
         need . concat $ [ [ outDir </> src | src <- srcs ]
                         , [ outDir </> "ipcore_dir" </> xco | xco <- ipcores ]
@@ -30,12 +44,24 @@ xilinxRules XilinxConfig{..} outDir projName srcs ipcores = do
 
         xilinx "xtclsh" [projName <.> "tcl", "rebuild_project"]
 
-    outDir </> "*.tcl" %> do
-        mustache projCtxt
+    outDir </> "*.tcl" %> \out -> do
+        target <- case mtarget of
+            Just target -> return target
+            Nothing -> do
+                board <- fromMaybe (error "Please set BOARD in build.mk") <$> getConfig "BOARD"
+                return $ fromMaybe (error $ unwords ["Unknown board:", board]) $ M.lookup board boards
+        mustache (projCtxt target) out
   where
-    xilinx tool args = cmd (Cwd outDir) (xilinxRoot </> tool) args
+    xilinx tool args = do
+        root <- getConfig "XILINX_ROOT"
+        wrap <- getConfig "XILINX_WRAPPER"
+        let exe = case (wrap, root) of
+                (Just wrap, _) -> [wrap, tool]
+                (Nothing, Just root) -> [root </> tool]
+                (Nothing, Nothing) -> error "XILINX_ROOT or XILINX_WRAPPER must be set"
+        cmd (Cwd outDir) exe args
 
-    projCtxt = object $ [
+    projCtxt XilinxTarget{..} = object $ [
         "project" ~=  projName,
         "targetFamily" ~= targetFamily,
         "targetDevice" ~= targetDevice,
@@ -44,8 +70,6 @@ xilinxRules XilinxConfig{..} outDir projName srcs ipcores = do
         "ipcores" ~> (map (\n -> object ["name" ~> n]) $ map dropExtension ipcores),
         "srcs" ~> (map (\s -> object ["fileName" ~> s]) srcs)
         ]
-      where
-        XilinxTarget{..} = xilinxTarget
 
 mustache :: Value -> FilePath -> Action ()
 mustache ctxt target = do
